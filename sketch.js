023756371ef9fbb1684d7e52bcd221cada8c98b3
kabeerWadhwa <68274419+kabeerWadhwa@users.noreply.github.com>
2020-08-18 15:39:23 +0100

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -7,7 +7,7 @@ const Body = Matter.Body;
 var engine,world
 function preload()
 {
-dustbinIMG.loadImage("Sprites/dustbingreen.png")
+dustbinIMG = loadImage("Sprites/dustbingreen.png")
 }
 
 function setup() {
@@ -17,10 +17,11 @@ function setup() {
 	world = engine.world;
 
 	//Create the Bodies Here
-	var dustbin1= createSprite(610,580,20,100);
-	var dustbin2= createSprite(650,630,100,20);
+	//var dustbin1= createSprite(610,580,20,100);
+	//var dustbin2= createSprite(650,630,100,20);
 	var dustbin3= createSprite(690,580,20,100);
 	dustbin3.addImage(dustbinIMG)
+	dustbin3.scale = 0.5
 	ground1 = new ground(400,650,800,20);
 	paper1 = new Paper(200,650,20,20);
 	Engine.run(engine);
@@ -28,9 +29,15 @@ function setup() {
 
 function draw() {
   rectMode(CENTER);
-  background(0);
+  background(255,255,255);
   Engine.update(engine)
   ground1.display()
   paper1.display()
   drawSprites();
+}
+
+function keyPressed() {
+	if (keyCode === UP_ARROW) {
+		Matter.Body.applyForce(paper1.body,paper1.body.position,{x:85, y:-85})
+	}
 }
